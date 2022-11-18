9e795b4dd6db9ae1e7bf75cf97acb48445686f3d
Crosby Johnson <s017812@students.lmsd.org>
2022-11-18 13:11:12 -0500

    djnfknjaksnj

--- New Wave Store/ContentView.swift
+++ New Wave Store/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("goodbye, world!")
+        Text("goodbye cruel world, world!")
             .padding()
     }
 }
